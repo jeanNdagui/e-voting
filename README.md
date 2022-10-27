f9ce0d95bae4940d780fe9df41d90e6cad44e58b
jeanNdagui <ndaguijeanlouismelanie@gmail.com>
2022-10-27 20:12:41 +0100

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tp-vote
-TP de sécurité informatique# tp-vote
+# e-vote
+In this project, we have made an small implementation in php to show how to use Blockchain technologie in vote system. The code is very basic we do not provide any comment about but if you have any suggestion contact us at ndaguijeanlouismelanie@gmail.com
--- src/views/home.php
+++ src/views/home.php
@@ -3,7 +3,18 @@
         <div class="card-body">
             <h2 class="card-title text-center my-3">Système de vote en ligne basé sur la technologie Blockchain</h2>
             
+             <p>Pour effectuer un vote, vous devez suivre les étapes ci-après:
+                <ol>
+                    <li>Créer un compte </li>
+                    <li>Cliquer sur le lien voter, choisissez votre candidat puis validez votre vote avec votre clé.</li>
+                </ol>
+             </p>
 
+             <p>
+                L'onglet résultat vous permettra de visualiser les résultats du vote en live.<br>
+                L'onglet Électeurs vous permettra de consulter la liste des différents électeurs<br>
+                L'onglet Canditats permettra de consulter celle des différents candidats
+             </p>
         </div>
     </div>
 </section>
